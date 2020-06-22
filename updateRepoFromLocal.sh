3deb52db13d3d3b71d2c36ee1a86249086d451b2
Josef Rysanek <josef.rysanek@gmail.com>
2020-06-22 10:20:57 +0200

    Update script from local machine updated with info message.

--- updateRepoFromLocal.sh
+++ updateRepoFromLocal.sh
@@ -2,8 +2,19 @@
 
 FILES=(.aliases .gitconfig .gitignore_global .vimrc .zshrc)
 
+echo "This script will update files in this repository with files from local machine:"
+echo "-------------"
+echo $FILES
+echo "-------------"
+echo "Should I continue?"
+read a
+
 for f in ${FILES[@]}
 do
  cp -va ~/${f} .
 done
+
+echo
+echo "Do NOT forget to commit and pull this change."
+echo
 
