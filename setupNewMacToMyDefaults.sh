9362d075b5c493155f0f4063f70fa0546f457141
Josef Rysanek <josef.rysanek@gmail.com>
2024-11-16 01:54:52 +0100

    Setup script updated.

--- setupNewMacToMyDefaults.sh
+++ setupNewMacToMyDefaults.sh
@@ -185,6 +185,8 @@ if [[ $1 =~ ^s ]]
   echo "Open and save files as UTF-8 in TextEdit"
   defaults write com.apple.TextEdit PlainTextEncoding -int 4
   defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+  echo "ENABLE FireWall"
+  sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
   echo ""
 
 # Hidden files
